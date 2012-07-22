bbf7e22e910a1841a846720ba28435b9865f8d09
Jacob Clark <jacob.jh.clark@googlemail.com>
2012-07-22 12:28:33 +0000

    Security Improvement

--- setup.php
+++ setup.php
@@ -1,10 +1,10 @@
 <?php
 $output = shell_exec('sudo mkdir /etc/raspcontrol');
 shell_exec('sudo chown root:root /etc/raspcontrol');
-shell_exec('sudo chmod 0777 /etc/raspcontrol');
+shell_exec('sudo chmod 0755 /etc/raspcontrol');
 shell_exec('sudo touch /etc/raspcontrol/database.aptmnt');
 shell_exec('sudo chown root:root /etc/raspcontrol/database.aptmnt');
-shell_exec('sudo chmod 0777 /etc/raspcontrol/database.aptmnt');
+shell_exec('sudo chmod 0755 /etc/raspcontrol/database.aptmnt');
 echo "<pre>$output</pre>";
 $myFile = "/etc/raspcontrol/database.aptmnt";
 $fh = fopen($myFile, 'w') or die("can't open file");
