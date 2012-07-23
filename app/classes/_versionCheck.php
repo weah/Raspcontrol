bf1648685d68515c493f9f9f6a26fda4cd97ca37
Jacob Clark <jacob.jh.clark@googlemail.com>
2012-07-23 22:33:25 +0000

    Version Controller Fixes

--- app/classes/_versionCheck.php
+++ app/classes/_versionCheck.php
@@ -1,7 +1,7 @@
 <?php
 	class versionCheck {
 		function checkVersion(){
-		    $version = "1"; //Version of the script, to check against CSV
+		    $version = "1.1"; //Version of the script, to check against CSV
 		    $critical = FALSE; //Set Critical Variable to False 
 		    $update = FALSE; //Set None Critical to Fasle too
 		    $url = "http://fusionstrike.com/raspcontrol/version.csv"; //Link to your external CSV to check against
@@ -11,9 +11,9 @@
 		    if ($read[0] > $version && $read[2] == "1") { $critical = TRUE; } // If its 1, set ciritcal to true
 		    if ($read[0] > $version) { $update = TRUE; } // Anything other than 1 set update to true
 		    if ($critical) { 
-		        print '<font color="red">There is a critical update available!</font><br/>You can get it at <a href="'.$read[3].'">'.$read[3].'</a> (Version: '.$read[1].') <br/><br/>';
+		        print '<font color="red">There is a critical update available (Version: '.$read[0].')!</font><br/>You can get it at <a href="'.$read[3].'">'.$read[3].'</a> (Description: '.$read[1].') <br/><br/>';
 		    }else if ($update){
-		        print '<font color="green">There is a none critical update available!</font><br/>You can get it at <a href="'.$read[3].'">'.$read[3].'</a> (Version: '.$read[1].') <br/><br/>';
+		        print '<font color="green">There is a none critical update available (Version: '.$read[0].')!</font><br/>You can get it at <a href="'.$read[3].'">'.$read[3].'</a> (Description: '.$read[1].') <br/><br/>';
 		    }
 			
 		}
