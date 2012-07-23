4a9073ebfcbe367a84afad228605157795669b2b
Jacob Clark <jacob.jh.clark@googlemail.com>
2012-07-23 20:23:43 +0000

    Refactoring

--- app/includes/_footer.php
+++ app/includes/_footer.php
@@ -1,6 +1,13 @@
 <div id="footerContainer">
         <div class="footerWrapper">
-            Powered by Raspcontrol (<!--<a href="settings.php">Settings</a>-->) &middot; <a href="http://fusionstrike.com">Fusion Strike</a> &middot; <a href="logout.php">Logout</a>
+            Powered by Raspcontrol (<!--<a href="settings.php">Settings</a>-->) &middot; <a href="http://fusionstrike.com">Fusion Strike</a> 
+            
+            <?php
+            if($_SESSION['username'] == ""){}else{
+	            echo '&middot; <a href="app/classes/_logout.php">Logout</a>';
+            }
+            ?>
+            
         </div>
     </div>
 </body>
